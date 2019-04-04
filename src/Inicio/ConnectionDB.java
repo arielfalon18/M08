6f977136b8c4287a115754ac97056d41bd3a44a8
arielfalon18 <ariel.falon@iesjoandaustria.org>
2019-04-04 20:12:30 +0200

    ConnectionDB.java
    ConnectionDB.java:change the err by logger

--- src/Inicio/ConnectionDB.java
+++ src/Inicio/ConnectionDB.java
@@ -1,17 +1,20 @@
 package Inicio;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConnectionDB {
 	static String DRIVER="org.hsqldb.jdbcDriver";
 	static String URL="jdbc:hsqldb:file:C:\\Users\\Super\\eclipse-workspace\\TiendaM08\\WebContent\\WEB-INF\\lib\\srv_db.tmp";
 		public static Connection getConection() {
 			Connection conn=null;
+			final Logger LOGGER = Logger.getLogger("Inicio");
 			try {
 				Class.forName(DRIVER);
 				conn=DriverManager.getConnection(URL, "SA", "");
 			} catch (ClassNotFoundException | SQLException e) {
-				System.err.println("ERROR "+e);
+				LOGGER.log(Level.SEVERE, e.toString());
 			}
 			return conn;
 			
