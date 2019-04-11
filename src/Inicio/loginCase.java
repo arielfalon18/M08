e2fe0f01be43132cde6c29da495fd32d2c6d12da
arielfalon18 <ariel.falon@iesjoandaustria.org>
2019-04-11 20:41:23 +0200

    loginCase.java:We put an empty constructor
    We put an empty constructor

--- src/Inicio/loginCase.java
+++ src/Inicio/loginCase.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import javax.servlet.http.HttpServletResponse;
 
 public class loginCase {
+	private loginCase() {}
 	public static boolean insertUser(String user,String upass, String uEmail) throws SQLException {
 		Connection con;
 		
